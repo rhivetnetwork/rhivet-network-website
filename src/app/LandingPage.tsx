d7c6e90922a1fe10ce8f0a02363292484e534faa
Sendy Inieke <ini.e.sendy@gmail.com>
2024-04-01 18:13:28 +0100

    [FeatHeroPage]: homePage

---
+++ src/app/LandingPage.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/Hero";
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default LandingPage;
